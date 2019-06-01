c07ebcfa42a9df75b645d9a449f7cd0e4538b672
Tai <rich77926@gmail.com>
2019-06-02 02:57:14 +0800

    Default hello world

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
 var express = require('express');
-var linebot = require('linebot');
+var app = express();
 
-// 用於辨識Line Channel的資訊
-var bot = linebot({
-  channelId: '1580401641',
-  channelSecret: 'd0e15a3c41a0a13913004e50d89bd9c6',
-  channelAccessToken: 'Ght2+eMo4wGplzahPLdRHOjxPNlaxGMM70dJtCMh6VybvDMiXHUfrkrasr1WSIlh10ZmVG2VOsoDWmEkpWEjYKVWT2ZSjYn4ghpajLepIA1rcHBuqlOeFXhzqIuB3ui0NvZapuRWAxxpi3hdVC0JTAdB04t89/1O/w1cDnyilFU='
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-// 當有人傳送訊息給Bot時
-bot.on('message', function (event) {
-  // event.message.text是使用者傳給bot的訊息
-  // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
-  event.reply(event.message.text).then(function (data) {
-    // 當訊息成功回傳後的處理
-  }).catch(function (error) {
-    // 當訊息回傳失敗後的處理
-  });
-});
-
-const app = express();
-const linebotParser = bot.parser();
-app.post('/', linebotParser);
-
-var server = app.listen(process.env.PORT || 8080, function() {
-  var port = server.address().port;
-  console.log('目前的port是', port);
+app.listen(443, function () {
+  console.log('Example app listening on port 3000!');
 });
