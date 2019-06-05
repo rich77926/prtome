e35ac0cc8d101bbf25c10a87c6559a8b6442b028
Tai <rich77926@gmail.com>
2019-06-05 15:28:07 +0800

    efefef

--- index.js
+++ index.js
@@ -7,24 +7,6 @@ var bot = linebot({
     channelAccessToken: 'jZZH8wMNFC52QX6W9+W6u4+fE73q1966j50Zk/jpDXiyK3E5DLOXoaGTcfBvy/grgdPWTCtSpU5qgW9tQHwWElxmE3kXTpzkvBhEcoFnFUSITeCEQ9RBInXumy9TiIWPuv3Q3Hcl0GEchIzubSgARwdB04t89/1O/w1cDnyilFU='
 });
 
-//取得使用者回覆的訊息
-// bot.on('message', function (event) {
-//     if (event.message.type = 'text') {
-//         // var msg = event.message.text;
-
-//         console.log(event.source.groupId);
-
-//         //重覆使用者說的訊息
-//         // event.reply("Test").then(function (data) {
-//         //     // success
-//         //     // console.log(event);
-//         // }).catch(function (error) {
-//         //     // error
-//         //     console.log('error:'+error);
-//         // });
-//     }
-// });
-
 const app = express();
 const linebotParser = bot.parser();
 
@@ -35,19 +17,10 @@ app.get("/", function (req, res) {
 app.use('/devops', express.json());
 app.post('/devops', function (req, res) {
     var userId = 'Cf76da8bb9560777af485a8a2fbeffe42';
-    var sendMsg = req.body.message.text + ' '+ req.body.resource.url;
+    var sendMsg = req.body.resource.createdBy.displayName + ' created a PR, approve it by : '+ req.body.resource._links.web;
     bot.push(userId, [sendMsg]).catch(function(err){console.log(err)});
     res.send(sendMsg);
 });
-
-// 主動發送訊息
-// setTimeout(function () {
-//     var userId = 'Uae110a6760ecc0163e88555fa46a90b2';
-//     var sendMsg = "push hands up ";
-//     bot.push(userId, [sendMsg]);
-//     console.log('userId: ' + userId);
-//     console.log('send: ' + sendMsg);
-// }, 3000);
 
 app.post('/linebot', linebotParser);
 
