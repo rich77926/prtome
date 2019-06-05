3df6ca64084bf45e0946559c0d12052855452a11
Tai <rich77926@gmail.com>
2019-06-05 11:43:17 +0800

    line

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const linebotParser = bot.parser();
 app.get("/", function (req, res) { 
     res.send("Hello LineBot");
 });
-app.post('/linebot/', linebotParser);
+app.post('/linebot', linebotParser);
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function () {
